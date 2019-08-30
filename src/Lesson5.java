a4508f222f4fbaf7b2a6dc7691a98a019d030adf
Danil <lilkit1996@gmail.com>
2019-08-30 16:01:24 +0300

    1

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-
 public class Lesson5 {
 
     Scanner scanner = new Scanner(System.in);
@@ -11,24 +10,23 @@ public class Lesson5 {
 
         // 1. Найти минимальное значение из массива
 
-        int [] cats = new int [11];
-      for (int i = 0; i < cats.length ; i++) {
-           cats[i] = (int) (Math.random() * 10);
-       }
-      System.out.println(Arrays.toString(cats));
-      int min = cats[0];
-        for (int i = 0; i <cats.length ; ++i) {
-              if (cats[i] < min)
-                  min = cats[i];
+        int[] cats = new int[11];
+        for (int i = 0; i < cats.length; i++) {
+            cats[i] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(cats));
+        int min = cats[0];
+        for (int i = 0; i < cats.length; ++i) {
+            if (cats[i] < min)
+                min = cats[i];
         }
         System.out.println("Минимальное число: " + min);
 
         //2. Переставить элементы массива в обратном порядке
         int[] arrayCats = new int[cats.length];
-        int count = cats.length - 1 ;
-        for (int i = 0; i < cats.length ; i++) {
-            arrayCats[i] = cats[count];
-            count--;
+        for (int i = 0; i < cats.length; i++) {
+            arrayCats[i] = cats[(cats.length - 1) - i];
+
 
         }
         System.out.println("Обратный порядок: " + Arrays.toString(arrayCats));
@@ -40,14 +38,13 @@ public class Lesson5 {
 
         String reverse = "";
         int length = scr.length();
-        for (int i = length - 1; i >=0 ; i--) {
+        for (int i = length - 1; i >= 0; i--) {
             reverse = reverse + scr.charAt(i);
 
         }
-        if (scr.equals(reverse)){
+        if (scr.equals(reverse)) {
             System.out.println("Это палиндром");
-        }
-        else {
+        } else {
             System.out.println("Это не полиндром");
         }
 
