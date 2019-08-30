de7e3d7c5472a7a7a73bf612471f3eb6b576f5e2
Danil <lilkit1996@gmail.com>
2019-08-30 14:10:47 +0300

    1

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
+
 public class Lesson5 {
 
     Scanner scanner = new Scanner(System.in);
@@ -11,7 +13,7 @@ public class Lesson5 {
 
         int [] cats = new int [11];
       for (int i = 0; i < cats.length ; i++) {
-           cats[i] = i;
+           cats[i] = (int) (Math.random() * 10);
        }
       System.out.println(Arrays.toString(cats));
       int min = cats[0];
@@ -22,31 +24,27 @@ public class Lesson5 {
         System.out.println("Минимальное число: " + min);
 
         //2. Переставить элементы массива в обратном порядке
-
-        int count = cats.length - 1;
-        for (int i = 0; i < cats.length; i++) {
-            cats[i] = count;
+        int[] arrayCats = new int[cats.length];
+        int count = cats.length - 1 ;
+        for (int i = 0; i < cats.length ; i++) {
+            arrayCats[i] = cats[count];
             count--;
+
         }
-        System.out.println("Обратный порядок: " + Arrays.toString(cats));
+        System.out.println("Обратный порядок: " + Arrays.toString(arrayCats));
 
         //3.Напишите программу, которая принимает входные данные...
 
         System.out.println("Укажите слово");
         String scr = scanner.nextLine();
 
-        char[] array = scr.toCharArray();
-        char[] array1 = new char[array.length];
-        int count1 = 1;
+        String reverse = "";
+        int length = scr.length();
+        for (int i = length - 1; i >=0 ; i--) {
+            reverse = reverse + scr.charAt(i);
 
-        for (int i = 0; i < array.length; i++) {
-           array1[i] = array[array.length - count1];
-           count1++;
         }
-
-        String str = new String(array1);
-
-        if (scr.equals(str)){
+        if (scr.equals(reverse)){
             System.out.println("Это палиндром");
         }
         else {
